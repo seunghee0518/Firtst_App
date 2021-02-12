809bfb275c776cf62591b62307cae1f0eb4c0b75
seunghee <shee6185@gmail.com>
2021-02-12 23:20:50 +0900

    text 배치 및 수정1

--- app/src/main/java/First/App/MainActivity.kt
+++ app/src/main/java/First/App/MainActivity.kt
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {  //앱이 최초 실행될 때 수행됨
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  //화면 뷰 연결(activity_main.xml 파일 연결됨)
+
+        tv_title.setText("Hello world!")  //setText = 텍스트 값 변경
     }
 }
