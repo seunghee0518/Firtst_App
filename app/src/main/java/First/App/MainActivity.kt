feb3271d96bfae27253a3c24ecd048b446bb836c
seunghee <shee6185@gmail.com>
2021-02-15 15:07:07 +0900

    로그인 화면& 뷰바인딩 코드
    로그인화면 텍스트, 버튼 배치
    뷰바인딩 코드 삽입

--- app/src/main/java/First/App/MainActivity.kt
+++ app/src/main/java/First/App/MainActivity.kt
@@ -7,11 +7,13 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {  //앱이 최초 실행될 때 수행됨
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)  //화면 뷰 연결(activity_main.xml 파일 연결됨)
+        setContentView(R.layout.activity_main)  //화면 뷰 연결(activity_main.xml 파일 연결됨
 
-        tv_title.setText("Hello world!")  //setText = 텍스트 값 변경
+        btn_getLogin.setOnClickListener {
+
+
+        }
     }
 }
